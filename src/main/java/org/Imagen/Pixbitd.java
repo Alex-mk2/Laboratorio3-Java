b67bbdd18dffb36995a59968237909f7774e5f1f
Alex-mk2 <75042499+Alex-mk2@users.noreply.github.com>
2022-11-14 23:40:47 -0300

    Comienzo laboratorio Java
    Se adjuntan los primeros archivos para trabajar con java

---
+++ src/main/java/org/Imagen/Pixbitd.java
@@ -0,0 +1,10 @@
+package org.Imagen;
+
+
+//TDA pixbit-d
+//Dom: X(int) X Y(int) X Bit(0 o 1) X Depth(int)
+//Recorrido: Pixel de tipo pixbit-d
+
+public class Pixbitd {
+
+}
---
+++ src/main/java/org/Imagen/Pixhexd.java
@@ -0,0 +1,4 @@
+package org.Imagen;
+
+public class Pixhexd {
+}
